08045c0a4de4a9b30fe37aefd18ad29c588a626f
Joao Pedro Cadinelli <jpcadinelli@gmail.com>
2023-10-04 12:38:57 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -6,44 +6,54 @@ const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
 const musicaFocoInput = document.querySelector('#alternar-musica');
-const musica = new Audio('/sons/luna-rise-part-one.mp3');
 const startPauseBt = document.querySelector('#start-pause');
-const audioPlay = new Audio('/sons/play.wav')
-const audioBeep = new Audio('/sons/beep.mp3')
-const audioPause = new Audio('/sons/pause.mp3')
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const imgPlayPause = document.querySelector('.app__card-primary-butto-icon');
+const tempoNaTela = document.querySelector('#timer');
+
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const audioPlay = new Audio('/sons/play.wav');
+const audioBeep = new Audio('/sons/beep.mp3');
+const audioPause = new Audio('/sons/pause.mp3');
 
 
-let tempoDecorridoEmSegundos = 5;
+let tempoDecorridoEmSegundos = 1500;
 let intervaloId = null;
 
 musica.loop = true;
 
 musicaFocoInput.addEventListener('change', () => {
     if(musica.paused) {
-        musica.play()
+        musica.play();
     } else {
-        musica.pause()
+        musica.pause();
     }
 });
 
 focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500;
+    mostrarTempo();
     alterarContexto('foco');
     focoBt.classList.add('active');
 });
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300;
+    mostrarTempo();
     alterarContexto('descanso-curto');
     curtoBt.classList.add('active');
 });
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900;
+    mostrarTempo();
     alterarContexto('descanso-longo');
     longoBt.classList.add('active');
 });
 
 function alterarContexto(contexto) {
     botoes.forEach(function (contexto){
-        contexto.classList.remove('active')
+        contexto.classList.remove('active');
     });
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
@@ -51,17 +61,17 @@ function alterarContexto(contexto) {
         case "foco":
             titulo.innerHTML = `
                 Otimize sua produtividade,<br><strong class="app__title-strong">mergulhe no que importa.</strong>
-                `
+                `;
             break;
         case "descanso-curto": 
             titulo.innerHTML = `
                 Que tal dar uma respirada?<br><strong class="app__title-strong">Faça uma pausa curta!</strong>
-                `
+                `;
             break;
         case "descanso-longo":
             titulo.innerHTML = `
                 Hora de voltar à superfície.<br><strong class="app__title-strong">Faça uma pausa longa.</strong>
-                `
+                `;
             break;
     };
 };
@@ -69,11 +79,11 @@ function alterarContexto(contexto) {
 const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0) {
         audioBeep.play();
-        zerar();
         alert('Tempo finalizado!');
+        zerar();
     };
     tempoDecorridoEmSegundos -=1;
-    console.log('Temporizador: ' + tempoDecorridoEmSegundos);
+    mostrarTempo();
 };
 
 startPauseBt.addEventListener('click', iniciar);
@@ -83,12 +93,24 @@ function iniciar() {
         zerar();
         audioPause.play();
         return;
-    }
+    };
     audioPlay.play();
     intervaloId = setInterval(contagemRegressiva, 1000);
+    iniciarOuPausarBt.textContent = "Pausar";
+    imgPlayPause.setAttribute('src', '/imagens/pause.png');
 };
 
 function zerar() {
     clearInterval(intervaloId);
+    iniciarOuPausarBt.textContent = "Começar";
+    imgPlayPause.setAttribute('src', '/imagens/play_arrow.png');
     intervaloId = null;
 };
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+    const tempoFormatado = tempo.toLocaleString('pt-Br', {minute: '2-digit', second: '2-digit'});
+    tempoNaTela.innerHTML = `${tempoFormatado}`;
+};
+
+mostrarTempo();
